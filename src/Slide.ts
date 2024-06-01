597be27f83d22abf6cf2d9b6ec3c6279a35e91ac
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-06-01 17:13:53 -0300

    feat: create buttons next and prev

--- src/Slide.ts
+++ src/Slide.ts
@@ -13,7 +13,8 @@ export default class Slide {
     this.time = time;
     this.index = 0;
     this.slide = this.slides[this.index]
-    this.showSlide(this.index); // Inicializa com o slide 2
+    this.initSlide()
+
   }
   hideSlide(element: Element) {
     element.classList.remove('active')
@@ -23,5 +24,31 @@ export default class Slide {
     this.slide = this.slides[this.index]
     this.slides.forEach(item => this.hideSlide(item));
     this.slide.classList.add("active");
+  }
+
+  prevSlide() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1
+    this.showSlide(prev)
+  }
+
+  nextSlide() {
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0
+    this.showSlide(next)
+  }
+
+  private addControls() {
+    const prevButton = document.createElement("button")
+    const nextButton = document.createElement("button")
+    prevButton.innerText = "Slide Anterior"
+    nextButton.innerText = "Próximo Slide"
+    this.controls.appendChild(prevButton)
+    this.controls.appendChild(nextButton)
+    prevButton.addEventListener("pointerup", () => this.prevSlide())
+    nextButton.addEventListener("pointerup", () => this.nextSlide())
+  }
+
+  private initSlide() {
+    this.showSlide(this.index); // Inicializa com o slide 2
+    this.addControls()
   }
 }
