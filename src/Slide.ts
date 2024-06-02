c1f74a69afa5c1ae6662e564ab3d4516a88e55ce
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-06-02 14:52:29 -0300

    feat: create a paused

--- src/Slide.ts
+++ src/Slide.ts
@@ -8,6 +8,8 @@ export default class Slide {
   index: number;
   slide: Element
   timeout: Timeout | null
+  pausedTimeout: Timeout | null
+  paused: boolean
 
   constructor(container: Element, slides: Element[], controls: Element, time: number = 5000) {
     this.container = container;
@@ -18,6 +20,8 @@ export default class Slide {
     this.slide = this.slides[this.index]
     this.initSlide() //Assim que cria o newSlide esta função é ativada
     this.timeout = null
+    this.pausedTimeout = null
+    this.paused = false
   }
 
   hideSlide(element: Element) {
@@ -38,13 +42,30 @@ export default class Slide {
   }
 
   prevSlide() {
+    if (this.paused) return
     const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1 //this.slides.length usado para ser o index do último slide
     this.showSlide(prev)
   }
 
   nextSlide() {
+    if (this.paused) return
     const next = this.index + 1 < this.slides.length ? this.index + 1 : 0
     this.showSlide(next)
+  }
+
+  pauseSlide() {
+    this.pausedTimeout = new Timeout(() => {
+      this.timeout?.pause()
+      this.paused = true
+    }, 300)
+  }
+
+  continueSlide() {
+    this.pausedTimeout?.clear()
+    if (this.paused) {
+      this.paused = false
+      this.timeout?.continue()
+    }
   }
 
   private addControls() {
@@ -56,6 +77,8 @@ export default class Slide {
     this.controls.appendChild(nextButton)
     prevButton.addEventListener("pointerup", () => this.prevSlide())
     nextButton.addEventListener("pointerup", () => this.nextSlide())
+    this.controls.addEventListener("pointerdown", () => this.pauseSlide())
+    this.controls.addEventListener("pointerup", () => this.continueSlide())
   }
 
   private initSlide() {
